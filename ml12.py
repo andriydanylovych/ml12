3b6ecd181d3c1ec64409b3788353f7d1328b1d29
andriydanylovych <andriy.danylovych@gmail.com>
2024-02-29 21:22:08 +0200

    added test run

--- ml12.py
+++ ml12.py
@@ -3,171 +3,171 @@
 
 # In[367]:
 
-import numpy
-import math
-import csv
-import random
+import numpy as np
 
-def open_csv_file(file_name): # 1 label + 784 pixels; 256/60000 images
-    matrix = []
-    with open(file_name, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter='\t')
-        for row in reader:
-            matrix.append(row)
-    return matrix
-
-def normalize_pixels(matrix_int):
+def normalize_pixels(m):
     MAX_PX_VALUE: float = 256. # Max pixel value
-    train_float = numpy.zeros((len(matrix_int), len(matrix_int[0]) - 1))
-    for item in range (len(matrix_int)):
-        for pixel in range (len(matrix_int[0]) - 1):
-            train_float[item][pixel] = float(matrix_int[item][pixel + 1]) / MAX_PX_VALUE
+    train_float = np.zeros((len(m), len(m[0]) - 1))
+    for item in range (len(m)):
+        train_float[item] = m[item][1:] / MAX_PX_VALUE
     return train_float   # only normalized pixels, labels excluded
 
-def save_weights_biases(weights_biases):
-    with open('C:/Users/andri/py/weights_biases.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, dialect='excel', delimiter='\t', quotechar='"')
-        for row in weights_biases:
-            writer.writerow(row)
+def length_weights_biases(NN):
+    return np.sum(NN[:-1]) + len(NN) - 1  # 819 = 3 + 784 + 16 + 16
+    
+def column_b_indices(NN):
+    col_b = [0]
+    for n in range (1, len(NN) - 1):  # n = 1, 2
+        col_b.append(col_b[n - 1] + NN[n - 1] + 1)
+    return col_b  # [0, 785, 802]
 
-def random_weights_biases(NN):
-    dim: int = 0
-    for d in range (len(NN)):
-        dim += (NN[d] + 1) # weights + biases
-    return [[(random.random() * 2. - 1.) for col in range(dim)] for row in range(NN[1])]
+def carve_out_weights(NN, weights_biases):
+    w = create_zero_weights(NN)
+    col_b = column_b_indices(NN)
+    for n in range (len(NN) - 1):  # n = 0, 1, 2
+        for row in range(NN[n + 1]):
+            for col in range(NN[n]):
+                w[n][row][col] = float(weights_biases[row][col + col_b[n] + 1])
+    return w
+
+def carve_out_biases(NN, weights_biases):
+    b = create_zero_biases(NN)
+    col_b = column_b_indices(NN)
+    for n in range (len(NN) - 1):  # n = 0, 1, 2
+        for row in range(NN[n + 1]):
+            b[n][row] = float(weights_biases[row][col_b[n]])
+    return b
 
 def sigmoid(a, w, b):
-    z = numpy.dot(a, w) + b
+    z = np.dot(a, w) + b
     if (z < -20):
-        return 0.
-    else:
-        return (1. / (1. + math.exp(-z)))
+        z = -20
+    return (1. / (1. + 2.718282 ** (-z)))
 
-def copy_iteration(m, m2) -> None:
-    for n in range (len(m)): # n = 0, 1, 2
-        m2[n][:] = m[n]
+def build_target_vector(label):  # label = digit
+    target_vector = np.zeros(10)  # 10 = NN[-1], number of labels
+    target_vector[label] = 1  # train_base[record][0] --> assigned label
+    return target_vector
     
+def create_zero_weights(NN):
+    w = []
+    for n in range (len(NN) - 1):  # n = 0, 1, 2
+        w.append(np.zeros((NN[n + 1], NN[n])))
+    return w
+    
+def create_zero_biases(NN):
+    b = []
+    for n in range (len(NN) - 1):  # n = 0, 1, 2
+        b.append(np.zeros(NN[n + 1]))
+    return b
+    
+def compute_activations(NN, x, w, b):
+    # Activations a=sigma(z);  z(n)=ww(n)*aa(n-1)+bb(n);  dc/dw=delta*aa
+    a = create_zero_biases(NN)
+    for i in range (NN[1]):  # compute activations of the first layer
+        a[0][i] = sigmoid(x, w[0][i], b[0][i])
+    for n in range (1, len(NN) - 1):  # compute activations of the layers 1 and 2
+        for i in range (NN[n + 1]):
+            a[n][i] = sigmoid(a[n - 1], w[n][i], b[n][i])
+    return a
+
+def compute_cost(vector1, vector2):
+    diff = vector1 - vector2
+    return np.dot(diff, diff)
+
+def compute_delta(NN, t, a, w):  # t = target_vector, a = activations, w = weights
+    m: int = len(NN) - 2  # m = OUTPUT_LAYER = 2
+    d = create_zero_biases(NN)
+    for i in range (NN[-1]):  # compute delta for output layer, NN[-1] = 10
+        d[m][i] = 2 * (t[i] - a[m][i]) * a[m][i] * (1 - a[m][i])
+    for n in range (m - 1, -1, -1):  # compute delta for layers n = 1, 0
+        wt = np.transpose(w[n + 1])
+        for i in range (len(d[n])):
+            d[n][i] = np.dot(d[n + 1], wt[i]) * a[n][i] * (1 - a[n][i])
+    return d
+
+def compute_dcdw(NN, x, a, d):  # d = delta
+    dcdw = create_zero_weights(NN)
+    dcdw[0] = np.outer(d[0], x)
+    for n in range (1, len(NN) - 1):  # n = 1, 2
+        dcdw[n] = np.outer(d[n], a[n - 1])
+    return dcdw
+
+def assemble_weights_biases(NN, w, b):
+    weights_biases = np.zeros((NN[1], length_weights_biases(NN)))
+    col_b = column_b_indices(NN)
+    for n in range (len(NN) - 1): # n = 0, 1, 2
+        for row in range(NN[n + 1]):
+            weights_biases[row][col_b[n]] = b[n][row] 
+            for col in range(NN[n]):
+                weights_biases[row][col + col_b[n] + 1] = w[n][row][col] 
+    return weights_biases
+
+def test_run(NN, images, labels, w, b):  # images = train_float, labels = train_base
+    cost = 0.
+    for i in range (len(images)): # LOOP TRAIN_BASE of 60000 IMAGES
+        cost += compute_cost(compute_activations(NN, images[i], w, b)[len(NN) - 2], 
+                             build_target_vector(int(labels[i][0])))
+    return cost / float(len(images))
+
 def main() -> None:
-    NN = [784, 16, 16, 10] # set neural network dimensions, 784 will be overwritten
-    READ_WEIGHTS_BIASES: bool = True
+    my_path = 'C:/Users/andri/py/'
+    my_train_file = 'train60000.csv'
+    my_test_file = 'test128.csv'
+    my_state_file = 'weights_biases.csv'
+    ITERATIONS: int = 9  # set a wanted number of training steps
+    READ_WEIGHTS_BIASES: bool = True  # to read from a file or to generate from scratch
 
-    train_base = open_csv_file('C:/Users/andri/py/mnist.csv')
+    train_base = np.loadtxt(my_path + my_train_file, delimiter=',', dtype='int64')
     train_float = normalize_pixels(train_base)
-
-    NN[0] = len(train_float[0]) # assigning actual number of pixels as the first NN layer
+    
+    ETA: float = 4. / float(len(train_float))
+    NN = [len(train_float[0]), 16, 16, 10]  # set neural network dimensions, NN[0] = 784
+    
+    test_base = np.loadtxt(my_path + my_test_file, delimiter=',', dtype='int64')
+    test_float = normalize_pixels(test_base)
 
     if READ_WEIGHTS_BIASES:
-        weights_biases = open_csv_file('C:/Users/andri/py/weights_biases.csv')
+        weights_biases = np.loadtxt(my_path + my_state_file, delimiter=',')
     else:
-        weights_biases = random_weights_biases(NN)   
+        weights_biases = np.random.rand(NN[1], length_weights_biases(NN))
 
-    # To carve out ww[] and bb[] from weights_biases[].
-    ww, bb = [], [] # weights, biases
-    col_b = 0
-    for n in range (len(NN) - 1): # n = 0, 1, 2
-        ww.append(numpy.zeros((NN[n + 1], NN[n])))
-        bb.append(numpy.zeros(NN[n + 1]))
-        if (n > 0):
-            col_b += (NN[n - 1] + 1)
-        for row in range(NN[n + 1]):
-            bb[n][row] = float(weights_biases[row][col_b])
-            for col in range(NN[n]):
-                ww[n][row][col] = float(weights_biases[row][col + col_b + 1])
-
-    # Activations aa=sigma(z);  z(n)=ww(n)*aa(n-1)+bb(n);  dc/dw=delta*aa
-    aa, delta, dcdw = [], [], [] 
-    for n in range (len(NN) - 1): # n = 0, 1, 2
-        aa.append(numpy.zeros(NN[n + 1]))
-        delta.append(numpy.zeros(NN[n + 1]))
-        dcdw.append(numpy.zeros((NN[n + 1], NN[n])))
-   
-    # To make a copy
-    ww2, bb2, delta2, dcdw2 = [], [], [], []
-    for n in range (len(NN) - 1): # n = 0, 1, 2
-        ww2.append(numpy.zeros((NN[n + 1], NN[n])))
-        ww2[n][:] = ww[n]
-        bb2.append(numpy.zeros(NN[n + 1]))
-        bb2[n][:] = bb[n]
-        delta2.append(numpy.zeros(NN[n + 1]))
-        dcdw2.append(numpy.zeros((NN[n + 1], NN[n])))
-                
-    ##### ITERATE(5) | LOOP TRAIN_BASE IMAGES(256|60000)
-
-    ETA: float = 1.
-    NUMBER_LABELS: int = 10 # NN[len(NN) - 1]
-    OUTPUT_LAYER: int = 2 # len(NN) - 2
-    NUMBER_RECORDS: int = 256 # len(train_float) 256|60000 (record=image)
-    ITERATIONS: int = 99
+    ww = carve_out_weights(NN, weights_biases)
+    bb = carve_out_biases(NN, weights_biases)
+    accu_dcdw = create_zero_weights(NN)
+    accu_dcdb = create_zero_biases(NN)
 
     cost_previous: float = 9.9
 
     for iteration in range (ITERATIONS):
 
-        cost: float = 0.
+        for n in range (len(NN) - 1): # n = 0, 1, 2
+            ww[n] = ww[n] + accu_dcdw[n] * ETA
+            bb[n] = bb[n] + accu_dcdb[n] * ETA
+        cost = test_run(NN, train_float, train_base, ww, bb)
 
-        for record in range (NUMBER_RECORDS): # LOOP TRAIN_BASE of IMAGES(256|60000) 
-
-            target_vector = numpy.zeros(10)
-            target_vector[int(train_base[record][0])] = 1 # train_base[record][0] --> assigned label
-
-            for i in range (NN[1]): # compute activations of the first layer
-                aa[0][i] = sigmoid(train_float[record], ww[0][i], bb[0][i])
-                
-            for n in range (1, len(NN) - 1): # compute activations of the layers 1 and 2
-                for i in range (NN[n + 1]):
-                    aa[n][i] = sigmoid(aa[n - 1], ww[n][i], bb[n][i])
-                    
-            diff = aa[OUTPUT_LAYER] - target_vector
-            cost += numpy.dot(diff, diff)
-            
-            for i in range (NUMBER_LABELS): # compute delta for output layer
-                delta[OUTPUT_LAYER][i] = 2 * (target_vector[i] - aa[OUTPUT_LAYER][i]) * aa[OUTPUT_LAYER][i] * (1 - aa[OUTPUT_LAYER][i])
-
-            for n in range (OUTPUT_LAYER - 1, -1, -1): # compute delta for layers n = 1, 0
-                ww_transpose = numpy.transpose(ww[n + 1])
-                for i in range (len(delta[n])):
-                    delta[n][i] = numpy.dot(delta[n + 1], ww_transpose[i]) * aa[n][i] * (1 - aa[n][i])
-
-            # Accumulate dcdw for all records
-            dcdw[0] = numpy.add(dcdw[0], numpy.outer(delta[0], train_float[record]))
-            for n in range (1, len(NN) - 1): # n = 1, 2
-                dcdw[n] = numpy.add(dcdw[n], numpy.outer(delta[n], aa[n - 1]))
-
-        cost /= float(NUMBER_RECORDS)
-
-        if (cost <= cost_previous):
-            print("cost(", iteration, ") = ", cost, " ETA = ", ETA)
-            copy_iteration(ww, ww2)
-            copy_iteration(bb, bb2)
-            copy_iteration(delta, delta2)
-            copy_iteration(dcdw, dcdw2)
-            ETA *= 2.
+        if (cost < cost_previous):
+            print(f'cost({iteration}) = {cost:.5f} test_cost = {test_run(NN, test_float, test_base, ww, bb):.5f} ETA = {(ETA * float(len(train_float))):.5f}')
+            ETA *= 1.3
             cost_previous = cost
+            accu_dcdw = create_zero_weights(NN)
+            accu_dcdb = create_zero_biases(NN)
+            for record in range (len(train_float)): # LOOP TRAIN_BASE of IMAGES(256|60000)
+                target_vector = build_target_vector(int(train_base[record][0]))
+                aa = compute_activations(NN, train_float[record], ww, bb)
+                delta = compute_delta(NN, target_vector, aa, ww)
+                dcdw = compute_dcdw(NN, train_float[record], aa, delta)
+                for n in range (0, len(NN) - 1):
+                    accu_dcdw[n] = np.add(accu_dcdw[n], dcdw[n])
+                    accu_dcdb[n] = np.add(accu_dcdb[n], delta[n])
         else:
-            print("--- cost(", iteration, ") = ", cost, " ETA = ", ETA)
-            copy_iteration(ww2, ww)
-            copy_iteration(bb2, bb)
-            copy_iteration(delta2, delta)
-            copy_iteration(dcdw2, dcdw)
-            ETA /= 2.
-        if (iteration < ITERATIONS - 1):
-            for n in range (len(NN) - 1): # n = 0, 1, 2
-                ww[n] = ww[n] + dcdw[n] * ETA
-                bb[n] = bb[n] + delta[n] * ETA
-                dcdw[n].fill(0)
-        # End of ITERATIONS LOOP
+            print(f'--- cost({iteration}) = {cost:.5f} ETA = {(ETA * float(len(train_float))):.5f}')
+            for n in range (len(NN) - 1): # reverse adjustment
+                ww[n] = ww[n] - accu_dcdw[n] * ETA
+                bb[n] = bb[n] - accu_dcdb[n] * ETA
+            ETA *= .3
 
-    # To build and download (overwrite, if exists) final weights_biases[]
-    col_b = 0
-    for n in range (len(NN) - 1): # n = 0, 1, 2
-        if (n > 0):
-            col_b += (NN[n - 1] + 1)
-        for row in range(NN[n + 1]):
-            weights_biases[row][col_b] = bb[n][row] 
-            for col in range(NN[n]):
-                weights_biases[row][col + col_b + 1] = ww[n][row][col] 
-
-    save_weights_biases(weights_biases)
+    np.savetxt((my_path + my_state_file), assemble_weights_biases(NN, ww, bb), delimiter=",")
     
 if __name__ == '__main__':
     main()
